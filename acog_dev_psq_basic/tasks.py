110fb09f88f6c7f8537ad8930b231ef011d3112c
Nitesh Kumar Gupta <nitesh@NiteshAir.aganitha.ai>
2022-12-19 11:09:09 +0530

    complted initial setup

--- acog_dev_psq_basic/tasks.py
+++ acog_dev_psq_basic/tasks.py
@@ -2,6 +2,10 @@ from celery import shared_task
 
 from django_celery_beat.models import PeriodicTask, CrontabSchedule
 
+# all one defines here is a task that needs to be pushed to the queue
+
+
+
 @shared_task
 def count_times(num:int)->None:
     for _ in range(num):
